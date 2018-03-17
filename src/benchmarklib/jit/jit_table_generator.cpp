6b2fca92fe0d146c598c921fba780e255fc0a112
Johannes Frohnhofen <johannes@frohnhofen.com>
2018-03-17 10:03:32 +0100

    tables

--- src/benchmarklib/jit/jit_table_generator.cpp
+++ src/benchmarklib/jit/jit_table_generator.cpp
@@ -26,8 +26,9 @@ void JitTableGenerator::generate_and_store() {
                       [&](std::vector<size_t> indices) { return generator.random_number(0, 100000); }, [&](std::vector<size_t> indices) { return false; });
   add_nullable_column<int32_t>(table_scan, "G", cardinalities,
                                [&](std::vector<size_t> indices) { return generator.random_number(0, 100000); }, [&](std::vector<size_t> indices) { return false; });
- add_nullable_column<int32_t>(table_scan, "H", cardinalities,
+  add_nullable_column<int32_t>(table_scan, "H", cardinalities,
                                [&](std::vector<size_t> indices) { return generator.random_number(0, 100000); }, [&](std::vector<size_t> indices) { return false; });
+
   auto table_aggregate = std::make_shared<opossum::Table>();
   add_column<int32_t>(table_aggregate, "ID", cardinalities, [&](std::vector<size_t> indices) { return indices[0]; });
   add_column<int32_t>(table_aggregate, "A", cardinalities,
@@ -39,8 +40,31 @@ void JitTableGenerator::generate_and_store() {
   add_column<int32_t>(table_aggregate, "D", cardinalities,
                       [&](std::vector<size_t> indices) { return generator.random_number(0, 10); });
 
+  auto table_types = std::make_shared<opossum::Table>();
+  add_column<int32_t>(table_types, "I1", cardinalities,
+                      [&](std::vector<size_t> indices) { return generator.random_number(0, 10); });
+  add_column<int32_t>(table_types, "I2", cardinalities,
+                      [&](std::vector<size_t> indices) { return generator.random_number(0, 10); });
+  add_column<int64_t>(table_types, "L1", cardinalities,
+                      [&](std::vector<size_t> indices) { return generator.random_number(0, 10); });
+  add_column<int64_t>(table_types, "L2", cardinalities,
+                      [&](std::vector<size_t> indices) { return generator.random_number(0, 10); });
+  add_column<float>(table_types, "F1", cardinalities,
+                      [&](std::vector<size_t> indices) { return generator.random_number(0, 10); });
+  add_column<float>(table_types, "F2", cardinalities,
+                      [&](std::vector<size_t> indices) { return generator.random_number(0, 10); });
+  add_column<double>(table_types, "D1", cardinalities,
+                      [&](std::vector<size_t> indices) { return generator.random_number(0, 10); });
+  add_column<double>(table_types, "D2", cardinalities,
+                      [&](std::vector<size_t> indices) { return generator.random_number(0, 10); });
+  add_column<std::string>(table_types, "S1", cardinalities,
+                      [&](std::vector<size_t> indices) { return std::string(1, static_cast<char>(generator.random_number(65, 75))); });
+  add_column<std::string>(table_types, "S2", cardinalities,
+                      [&](std::vector<size_t> indices) { return std::string(1, static_cast<char>(generator.random_number(65, 75))); });
+
   StorageManager::get().add_table("TABLE_SCAN", table_scan);
   StorageManager::get().add_table("TABLE_AGGREGATE", table_aggregate);
+  StorageManager::get().add_table("TABLE_TYPES", table_types);
 }
 
 }  // namespace opossum
