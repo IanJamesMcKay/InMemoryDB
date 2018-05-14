d038bb2203ed8442b3e4a1d517adf6c3d542d6fa
Fabian Wiebe <fabian.wiebe@student.hpi.de>
2018-05-14 12:35:11 +0200

    run benchmark with scaling factor 0.5

--- run_dlp_benchmark.sh
+++ run_dlp_benchmark.sh
@@ -1,4 +1,4 @@
-SCALE=0.1
+SCALE=0.5
 ./cmake-build-release/hyriseBenchmarkTPCH --mvcc --scale $SCALE -o result_wo_pc_wo_rl.json
 ./cmake-build-release/hyriseBenchmarkTPCH --mvcc --punch_card --scale $SCALE -o result_w_pc_wo_rl.json
 ./cmake-build-release/hyriseBenchmarkTPCH --mvcc --rate_limiting --scale $SCALE -o result_wo_pc_w_rl.json
