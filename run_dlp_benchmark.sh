5a09c80dccc8783952c46e3ea86b03b274a755d7
Fabian Wiebe <fabian.wiebe@student.hpi.de>
2018-05-14 13:09:33 +0200

    added benchmark result for benchmark with permuted query sets

--- run_dlp_benchmark.sh
+++ run_dlp_benchmark.sh
@@ -1,5 +1,5 @@
 SCALE=0.5
-./cmake-build-release/hyriseBenchmarkTPCH --mvcc --scale $SCALE -o result_wo_pc_wo_rl.json
-./cmake-build-release/hyriseBenchmarkTPCH --mvcc --punch_card --scale $SCALE -o result_w_pc_wo_rl.json
-./cmake-build-release/hyriseBenchmarkTPCH --mvcc --rate_limiting --scale $SCALE -o result_wo_pc_w_rl.json
-./cmake-build-release/hyriseBenchmarkTPCH --mvcc --punch_card --rate_limiting --scale $SCALE -o result_w_pc_w_rl.json
+./cmake-build-release/hyriseBenchmarkTPCH -m PermutedQuerySets --mvcc --scale $SCALE -o result_wo_pc_wo_rl.json
+./cmake-build-release/hyriseBenchmarkTPCH -m PermutedQuerySets --mvcc --punch_card --scale $SCALE -o result_w_pc_wo_rl.json
+./cmake-build-release/hyriseBenchmarkTPCH -m PermutedQuerySets --mvcc --rate_limiting --scale $SCALE -o result_wo_pc_w_rl.json
+./cmake-build-release/hyriseBenchmarkTPCH -m PermutedQuerySets --mvcc --punch_card --rate_limiting --scale $SCALE -o result_w_pc_w_rl.json
