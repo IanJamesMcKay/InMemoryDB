14108f9b8ccd41e485a58d66a19decb4f1aa5c6a
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2018-06-07 18:18:06 +0200

    somethings

--- src/lib/optimizer/strategy/join_ordering_rule.cpp
+++ src/lib/optimizer/strategy/join_ordering_rule.cpp
@@ -1,13 +1,11 @@
 #include "join_ordering_rule.hpp"
 
 #include "optimizer/join_ordering/abstract_join_ordering_algorithm.hpp"
-#include "optimizer/join_ordering/abstract_join_plan_node.hpp"
 #include "optimizer/join_ordering/dp_ccp.hpp"
 #include "optimizer/join_ordering/join_graph_builder.hpp"
 #include "query_blocks/abstract_query_block.hpp"
 #include "query_blocks/query_blocks_from_lqp.hpp"
 #include "query_blocks/predicate_join_block.hpp"
-#include "query_blocks/stored_table_block.hpp"
 
 namespace opossum {
 
