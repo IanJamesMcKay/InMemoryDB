747e7e1f60a0796c2f40280d55781a2136cead44
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2018-06-12 09:55:00 +0200

    fix

--- src/lib/utils/format_duration.hpp
+++ src/lib/utils/format_duration.hpp
@@ -7,6 +7,6 @@
 namespace opossum {
 
 // "3h 42min" instead of "2,53×10¹²ns"
-std::string format_duration(const std::chrono::nanoseconds nanoseconds);
+std::string format_duration(const std::chrono::nanoseconds& nanoseconds);
 
 }  // namespace opossum
