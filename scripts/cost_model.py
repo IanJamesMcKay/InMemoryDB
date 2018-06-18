3778f453ba5e5f20c0f92b263b5d23290fda4c04
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2018-06-18 17:36:03 +0200

    scripted around a little

--- scripts/cost_model.py
+++ scripts/cost_model.py
@@ -1,40 +1,76 @@
 import json
 import pandas
 import sklearn.linear_model
+import sklearn.metrics
+import numpy as np
 import matplotlib.pyplot as plt
 
+def mean_absolute_percentage_error(y_true, y_pred):
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    abs_errors = np.abs((y_true - y_pred) / y_true)
+    return np.mean(abs_errors) * 100
+
+
 samples_path = "/home/moritz/pella/hyrise/joe/cost-sample/CostFeatureSamples-Release.json"
+
+features_by_operator = {
+    "JoinHash": ["MajorInputRowCount", "MinorInputRowCount", "OutputRowCount"],
+    "TableScan_Int": ["LeftInputRowCount", "LeftInputReferenceRowCount", "OutputRowCount"],
+    "TableScan_Long": ["LeftInputRowCount", "LeftInputReferenceRowCount", "OutputRowCount"],
+    "TableScan_Like": ["LeftInputRowCount", "LeftInputReferenceRowCount", "OutputRowCount"],
+}
+
 
 with open(samples_path) as file:
     samples_json = json.load(file)
 
-for operator, operator_samples_json in samples_json.items():
-    if operator != "JoinHash":
+samples = {}
+samples["TableScan_Int"] = []
+samples["TableScan_Long"] = []
+samples["TableScan_Like"] = []
+
+for key, operator_samples in samples_json.items():
+    if key == "TableScan":
+        for operator_sample in operator_samples:
+            if operator_sample["RightDataType"] == "int":
+                samples["TableScan_Int"].append(operator_sample)
+            elif operator_sample["RightDataType"] == "long":
+                samples["TableScan_Long"].append(operator_sample)
+            elif operator_sample["PredicateCondition"] == "LIKE":
+                samples["TableScan_Like"].append(operator_sample)
+
+    else:
+        samples[key] = operator_samples
+
+for operator, operator_samples in samples.items():
+    if operator not in features_by_operator:
         continue
 
-    print("{}: {} samples".format(operator, len(operator_samples_json)))
+    print("{}: {} samples".format(operator, len(operator_samples)))
 
-    df = pandas.DataFrame(operator_samples_json)
+    df = pandas.DataFrame(operator_samples)
     df = df[df.Runtime < 300*1000*1000]
     df = df[df.Runtime > 1000]
 
+    target = "Runtime"
+    features = features_by_operator[operator]
+
+    print(df.columns.values.tolist())
+
+    df = pandas.DataFrame(df, columns=features + [target])
+
     y = df["Runtime"]
-
-
-    X = pandas.DataFrame(df, columns=["MajorInputRowCount", "MinorInputRowCount", "OutputRowCount"])
-
+    X = pandas.DataFrame(df, columns=features)
 
     reg = sklearn.linear_model.Lars(fit_intercept=False, positive=True)
     model = reg.fit(X, y)
 
-    predictions = model.predict(X)
+    y_preds = model.predict(X)
 
+    #for idx, y_pred in enumerate(y_preds):
+    #    print(y.iloc[idx], int(y_pred))
 
-    for idx, y_pred in enumerate(predictions):
-        print(y.iloc[idx], int(y_pred))
-
-    print(model.score(X,y))
-    print(model.coef_)
+    print("  Coef: {}".format(model.coef_))
 
     #reg = sklearn.linear_model.LinearRegression(fit_intercept=False)
     #model = reg.fit(X, y)
@@ -43,7 +79,10 @@ for operator, operator_samples_json in samples_json.items():
    # ax.set_yscale('log')
    # ax.set_xscale('log')
 
-    plt.scatter(y, predictions, s=0.4)
+    print("  MAPE: {}".format(mean_absolute_percentage_error(y, y_preds)))
+    print("  R2: {}".format(sklearn.metrics.r2_score(y, y_preds)))
+
+    plt.scatter(y, y_preds, s=0.4)
     plt.show()
 
 
