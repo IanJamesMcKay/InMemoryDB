b98e84bac56206a19cb0ab15bcfb5644ae98c349
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2018-06-25 19:38:46 +0200

    some changes

--- src/benchmark/expression_presentation_benchmarks.cpp
+++ src/benchmark/expression_presentation_benchmarks.cpp
@@ -6,28 +6,51 @@
 #include "sql/sql_pipeline_builder.hpp"
 #include "sql/sql_pipeline.hpp"
 #include "sql/sql_pipeline_statement.hpp"
+#include "table_generator.hpp"
 
 namespace opossum {
 
 class ExpressionPresentationFixture : public benchmark::Fixture {
  public:
   void SetUp(benchmark::State &state) override {
-    TpchDbGenerator{0.1f}.generate_and_store();
   }
 
   void TearDown(benchmark::State &state) override {
-    StorageManager::reset();
   }
 
   const std::string query_arithmetics = "SELECT l_extendedprice*(1.0-l_discount)*(1.0+l_tax) FROM lineitem";
+  const std::string query_arithmetics_nullables = "SELECT a*(100-b)*(100+c) FROM t";
 };
 
 BENCHMARK_F(ExpressionPresentationFixture, Arithmetics)(benchmark::State& state) {
+  TpchDbGenerator{0.5f}.generate_and_store();
+
+  std::cout << "Num Rows: " << StorageManager::get().get_table("lineitem")->row_count() << std::endl;
+
   while (state.KeepRunning()) {
     auto pipeline_statement = SQLPipelineBuilder{query_arithmetics}.disable_mvcc().create_pipeline_statement();
     pipeline_statement.get_result_table();
   }
+
+  StorageManager::reset();
 }
+
+BENCHMARK_F(ExpressionPresentationFixture, ArithmeticsNullable)(benchmark::State& state) {
+  TableGenerator table_generator;
+  table_generator.num_columns = 3;
+  table_generator.num_rows = 3'000'000;
+
+  StorageManager::get().add_table("t", table_generator.generate_table(200'000, 0.3));
+
+  while (state.KeepRunning()) {
+    auto pipeline_statement = SQLPipelineBuilder{query_arithmetics_nullables}.disable_mvcc().create_pipeline_statement();
+    pipeline_statement.get_result_table();
+  }
+
+  StorageManager::reset();
+}
+
+
 
 //BENCHMARK_F(ExpressionPresentationFixture, Arithmetics_JIT)(benchmark::State& state) {
 //  while (state.KeepRunning()) {
